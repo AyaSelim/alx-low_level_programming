42a097b704cc9d2f842716e783b3c62af56887f3
AyaSelim <ayaselim474@gmail.com>
2023-02-16 09:24:15 -0800

    commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: 'the program's description' 
- * Return: Always 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
