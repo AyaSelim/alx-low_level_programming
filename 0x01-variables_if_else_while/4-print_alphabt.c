28f7416900121a3c8abfaeb7ae8785649620957e
AyaSelim <ayaselim474@gmail.com>
2023-02-17 09:32:13 -0800

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-lude <stdio.h>
+#include <stdio.h>
 /**
  * main - main block
  * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
@@ -8,12 +8,12 @@ int main(void)
 {
 char c = 'a';
 
-			while (c <= 'z')
-					{
-						if (c != 'q' && c != 'e')
-							putchar(c);
-							c++;
-					}
+while (c <= 'z')
+{
+	if (c != 'q' && c != 'e')
+		putchar(c);
+	c++;
+}
 putchar('\n');
 
 return (0);
