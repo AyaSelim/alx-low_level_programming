b8428e4a4b091116b741c825489da858cdd3328d
AyaSelim <ayaselim474@gmail.com>
2023-02-21 06:53:35 -0800

    comit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h
+#include <stdio.h>
+/**
+* main - main block
+* Description: print using -putchar
+* Return: 0
+*/
+int main(void)
+{
+
+
+
+char ch[9] = "_putchar";
+int i = 0;
+
+for (int i = 0; i <= 7; i++)
+{
+
+putchar(ch[i]);
+}
+return (0);
+
+
+
+
+
+}
